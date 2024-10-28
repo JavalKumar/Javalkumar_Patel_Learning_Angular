814c73bca1285284d6085e655088a782e2b919df
“Javalkumar <W0845066@myscc.ca>
2024-10-28 17:07:47 -0400

    Created two components

---
+++ src/app/dog-list-item/dog-list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dog-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './dog-list-item.component.html',
+  styleUrl: './dog-list-item.component.scss'
+})
+export class DogListItemComponent {
+
+}
---
+++ src/app/dog-list/dog-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dog-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './dog-list.component.html',
+  styleUrl: './dog-list.component.scss'
+})
+export class DogListComponent {
+
+}

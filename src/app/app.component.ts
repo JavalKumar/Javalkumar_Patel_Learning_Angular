9f71709224ca58e2cfb0d0fced2dc2c8b4995517
“Javalkumar <W0845066@myscc.ca>
2024-10-28 15:56:35 -0400

    Created HTML. Used div tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Dog } from './models/dog';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Javalkumar-Patel-Learning-Angular';
-  name: string = "Javalkumar Patel";
-  favCar: string = "VW Polo GTI"
+  dogs: Dog[] = [
+    {name: 'Rozy', breed: 'Dobberman', age: 13, weight: 30, isAdopted: false},
+    {name: 'Oreo', breed: 'German Shephard', age: 1, weight: 6, isAdopted: true},
+    {name: 'Preeti', breed: 'Lab', age: 5, weight: 25, isAdopted: true},
+    {name: 'Murphie', breed: 'Pug', age: 4, weight: 12, isAdopted: false},
+    {name: 'Staphy', breed: 'Pomerian', age: 7, weight: 15, isAdopted: false},
+    {name: 'Candy', breed: 'Pitbull', age: 5, weight: 28, isAdopted: true}
+  ]
 }

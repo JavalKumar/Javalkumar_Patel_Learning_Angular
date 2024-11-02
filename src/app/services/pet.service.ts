323ed4700647e27938cfe04feb88bae256a1ab9e
“Javalkumar <W0845066@myscc.ca>
2024-11-01 23:38:26 -0400

    Method Added

--- src/app/services/pet.service.ts
+++ src/app/services/pet.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
-
+import { Observable,of } from 'rxjs';
+import { dogs } from '../data/mock-content';
+import { Dog } from '../models/dog';
 @Injectable({
   providedIn: 'root'
 })
 export class PetService {
-
+  private pets: Dog[] = dogs;
   constructor() { }
+
+  getPet():Observable<Dog[]>{
+    return of(dogs);
+  }
+  addPet(newPet: Dog): Observable<Dog[]>{
+    this.pets.push(newPet);
+  }
+  updatePet(updatepet:Dog): Observable<Dog[]>{
+    const index = this.pets.findIndex(dogs=>dogs.name === this.updatePet.name);
+    if(index!== -1){
+      this.pets[index] = this.updatePet;
+    }
+
+  }
 }

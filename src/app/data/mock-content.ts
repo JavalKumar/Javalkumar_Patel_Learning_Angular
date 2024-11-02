495ee015dd68c19fefcba8d8d7ac2da575c2bb7a
“Javalkumar <W0845066@myscc.ca>
2024-11-01 23:15:34 -0400

    added new service using dependency injection

---
+++ src/app/data/mock-content.ts
@@ -0,0 +1,10 @@
+import { Dog } from "../models/dog";
+
+export const dogs: Dog[] = [
+    { name: 'Rozy', breed: 'Doberman', age: 13, weight: 30, isAdopted: false },
+    { name: 'Oreo', breed: 'German Shepherd', age: 1, weight: 6, isAdopted: true },
+    { name: 'Preeti', breed: 'Lab', age: 5, weight: 25, isAdopted: true },
+    { name: 'Murphie', breed: 'Pug', age: 4, weight: 12, isAdopted: false },
+    { name: 'Staphy', breed: 'Pomeranian', age: 7, weight: 15, isAdopted: false },
+    { name: 'Candy', breed: 'Pitbull', age: 5, weight: 28, isAdopted: true }
+  ];
--- src/app/dog-list/dog-list.component.ts
+++ src/app/dog-list/dog-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Dog } from '../models/dog';
 import { DogListItemComponent } from '../dog-list-item/dog-list-item.component';
 import { CommonModule } from '@angular/common';
+import { PetService } from '../services/pet.service';
 
 @Component({
   selector: 'app-dog-list',
@@ -12,14 +13,11 @@ import { CommonModule } from '@angular/common';
 })
 export class DogListComponent {
   displayedColumns:string[]= ['name', 'breed', 'age', 'weight', 'isAdopted'];
-  dogs: Dog[] = [
-    { name: 'Rozy', breed: 'Doberman', age: 13, weight: 30, isAdopted: false },
-    { name: 'Oreo', breed: 'German Shepherd', age: 1, weight: 6, isAdopted: true },
-    { name: 'Preeti', breed: 'Lab', age: 5, weight: 25, isAdopted: true },
-    { name: 'Murphie', breed: 'Pug', age: 4, weight: 12, isAdopted: false },
-    { name: 'Staphy', breed: 'Pomeranian', age: 7, weight: 15, isAdopted: false },
-    { name: 'Candy', breed: 'Pitbull', age: 5, weight: 28, isAdopted: true }
-  ];
+  dogs: Dog[] = [];
+
+  constructor(private petService:PetService){
+    
+  }
 
   selectedDog?: Dog;
 
